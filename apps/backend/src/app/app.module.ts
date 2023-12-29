6b87c37d1caa703cec508442da8fc73a7fbca33d
prawira <robbyprawira3@gmail.com>
2023-12-29 22:57:51 +0700

    Setup Database Connection

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthenticationModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/taskOrganizer'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
