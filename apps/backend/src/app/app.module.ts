e2be4d6403aaf0255eb57a2e788139edd01e4340
prawira <robbyprawira3@gmail.com>
2023-12-29 23:25:01 +0700

    feat: Integrate Configuration Files
    Integrate configuration file to main module and calling database URI
    from env file

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     AuthenticationModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/taskOrganizer'),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
