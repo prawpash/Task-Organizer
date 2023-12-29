8e159afd0ebeddaae60311a60bf785ccfe5d485f
prawpash <59055214+prawpash@users.noreply.github.com>
2023-12-29 23:31:13 +0700

    Feature: Setup Database And Configuration Module (#1)
    * Setup Database Connection
    
    * feat: New Configuration Package
    
    Add a package to manage configuration from env file
    
    * doc: env example
    
    Create example of env current env file
    
    * feat: Integrate Configuration Files
    
    Integrate configuration file to main module and calling database URI
    from env file

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    AuthenticationModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
