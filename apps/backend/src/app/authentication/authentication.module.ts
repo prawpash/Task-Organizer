a5d4500503b7f5cae46fc144b9dcd59d284c6f3e
prawira <robbyprawira3@gmail.com>
2024-02-04 11:41:46 +0700

    Feat: Register JWT Module
    Register JWT Module to authentication module

--- apps/backend/src/app/authentication/authentication.module.ts
+++ apps/backend/src/app/authentication/authentication.module.ts
@@ -3,9 +3,20 @@ import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 import { SchemasModule } from '../../schemas/schemas.module';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [SchemasModule, UserModule],
+  imports: [
+    SchemasModule,
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '60m',
+      },
+    }),
+  ],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
