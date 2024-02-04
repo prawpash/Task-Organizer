e00df53bb709e181201f4a3c789161d5cfb45f3d
prawira <robbyprawira3@gmail.com>
2024-02-04 11:43:10 +0700

    Feat: Login Controller
    Add a controller to managing path and process of login

--- apps/backend/src/app/authentication/authentication.controller.spec.ts
+++ apps/backend/src/app/authentication/authentication.controller.spec.ts
@@ -5,19 +5,28 @@ import { getModelToken } from '@nestjs/mongoose';
 import { User, UserDocument } from '../../schemas/user.schema';
 import { UserService } from '../user/user.service';
 import { RegisterAuthenticationDto } from './dto/register-authentication.dto';
+import { LoginAuthenticationDto } from './dto/login-authentication.dto';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { Types } from 'mongoose';
+import { verifyPassword } from '../../utils/hash';
+import { JwtService } from '@nestjs/jwt';
 
 jest.mock('../../utils/hash.ts', () => ({
   hashPassword: jest
     .fn()
     .mockImplementation(async (password) => `hashed_${password}`),
+  verifyPassword: jest
+    .fn()
+    .mockImplementation(
+      async (password, hashedPassword) => password == hashedPassword
+    ),
 }));
 
 describe('AuthenticationController', () => {
   let controller: AuthenticationController;
   let userService: UserService;
   let authenticationService: AuthenticationService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -25,6 +34,7 @@ describe('AuthenticationController', () => {
       providers: [
         AuthenticationService,
         UserService,
+        JwtService,
         {
           provide: getModelToken(User.name),
           useValue: {
@@ -39,63 +49,161 @@ describe('AuthenticationController', () => {
     authenticationService = module.get<AuthenticationService>(
       AuthenticationService
     );
+    jwtService = module.get<JwtService>(JwtService);
+
+    // Set ENV Variable
+    process.env.JWT_SECRET = 'myJwtSecret';
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should return error duplicate email', async () => {
-    jest.spyOn(userService, 'doesThisEmailExist').mockResolvedValue(true);
+  describe('Register Process', () => {
+    it('should return error duplicate email', async () => {
+      jest.spyOn(userService, 'doesThisEmailExist').mockResolvedValue(true);
 
-    jest.spyOn(authenticationService, 'register');
+      jest.spyOn(authenticationService, 'register');
 
-    const mockBody = {
-      email: 'rudolph@mail.com',
-      password: 'asdfghjkl',
-    } as RegisterAuthenticationDto;
+      const mockBody = {
+        email: 'rudolph@mail.com',
+        password: 'asdfghjkl',
+      } as RegisterAuthenticationDto;
 
-    try {
-      await controller.register(mockBody);
-    } catch (error) {
-      expect(error).toBeInstanceOf(HttpException);
-      expect(error.getStatus()).toBe(HttpStatus.CONFLICT);
-      expect(error.message).toBe('Data with this email already exists.');
-    }
+      try {
+        await controller.register(mockBody);
+      } catch (error) {
+        expect(error).toBeInstanceOf(HttpException);
+        expect(error.getStatus()).toBe(HttpStatus.CONFLICT);
+        expect(error.message).toBe('Data with this email already exists.');
+      }
 
-    expect(authenticationService.register).not.toHaveBeenCalled();
+      expect(authenticationService.register).not.toHaveBeenCalled();
+    });
+
+    it('should create new data', async () => {
+      const mockBody = {
+        name: 'rudolph',
+        email: 'rudolph@mail.com',
+        password: 'asdfghjkl',
+      } as RegisterAuthenticationDto;
+
+      jest.spyOn(userService, 'doesThisEmailExist').mockResolvedValue(false);
+
+      jest.spyOn(authenticationService, 'register').mockResolvedValue({
+        ...mockBody,
+        _id: 'lkjasdsalkj' as unknown as Types.ObjectId,
+      } as UserDocument);
+
+      const registrationProcess = await controller.register(mockBody);
+
+      expect(registrationProcess).toEqual({
+        _id: 'lkjasdsalkj',
+        name: mockBody.name,
+        email: mockBody.email,
+      });
+
+      expect(authenticationService.register).toHaveBeenCalled();
+      expect(userService.doesThisEmailExist).toHaveBeenCalledWith(
+        mockBody.email
+      );
+      expect(authenticationService.register).toHaveBeenCalledWith({
+        ...mockBody,
+        password: `hashed_${mockBody.password}`,
+      });
+
+      // @ts-ignore
+      expect(registrationProcess.password).toBeUndefined();
+    });
   });
 
-  it('should create new data', async () => {
-    const mockBody = {
-      name: 'rudolph',
-      email: 'rudolph@mail.com',
-      password: 'asdfghjkl',
-    } as RegisterAuthenticationDto;
+  describe('Login Process', () => {
+    describe('Wrong Email', () => {
+      it('Should Return Error Email Or Password Wrong', async () => {
+        const mockBody = {
+          email: 'wrong@mail.com',
+          password: 'wrongPass',
+        } as LoginAuthenticationDto;
 
-    jest.spyOn(userService, 'doesThisEmailExist').mockResolvedValue(false);
+        jest.spyOn(userService, 'findByEmail').mockResolvedValue(null);
 
-    jest.spyOn(authenticationService, 'register').mockResolvedValue({
-      ...mockBody,
-      _id: 'lkjasdsalkj' as unknown as Types.ObjectId,
-    } as UserDocument);
+        try {
+          const loginProcess = await controller.login(mockBody);
 
-    const registrationProcess = await controller.register(mockBody);
+          expect(loginProcess).toThrowError();
+        } catch (error) {
+          expect(error).toBeInstanceOf(HttpException);
+          expect(error.getStatus()).toBe(HttpStatus.UNAUTHORIZED);
+          expect(error.message).toBe('The Email or Password is wrong.');
+        }
 
-    expect(registrationProcess).toEqual({
-      _id: 'lkjasdsalkj',
-      name: mockBody.name,
-      email: mockBody.email,
+        expect(userService.findByEmail).toHaveBeenCalledWith(mockBody.email);
+        expect(verifyPassword).not.toHaveBeenCalled();
+      });
     });
 
-    expect(authenticationService.register).toHaveBeenCalled();
-    expect(userService.doesThisEmailExist).toHaveBeenCalledWith(mockBody.email);
-    expect(authenticationService.register).toHaveBeenCalledWith({
-      ...mockBody,
-      password: `hashed_${mockBody.password}`,
+    describe('Wrong Password', () => {
+      it('Should Return Error Email Or Password Wrong', async () => {
+        const mockUser = {
+          name: 'rudolph',
+          email: 'rudolph@mail.com',
+          password: 'hashed_password',
+        } as UserDocument;
+
+        const mockBody = {
+          email: 'rudolph@mail.com',
+          password: 'wrongPassword',
+        } as LoginAuthenticationDto;
+
+        jest.spyOn(userService, 'findByEmail').mockResolvedValue(mockUser);
+
+        try {
+          const loginProcess = await controller.login(mockBody);
+
+          expect(loginProcess).toThrowError();
+        } catch (error) {
+          expect(error).toBeInstanceOf(HttpException);
+          expect(error.getStatus()).toBe(HttpStatus.UNAUTHORIZED);
+          expect(error.message).toBe('The Email or Password is wrong.');
+        }
+
+        expect(userService.findByEmail).toHaveBeenCalledWith(mockBody.email);
+        expect(verifyPassword).toHaveBeenCalledWith(
+          mockBody.password,
+          mockUser.password
+        );
+      });
     });
 
-    // @ts-ignore
-    expect(registrationProcess.password).toBeUndefined();
+    it('should return access token', async () => {
+      const mockUser = {
+        name: 'rudolph',
+        email: 'rudolph@mail.com',
+        password: 'hashed_password',
+      } as UserDocument;
+
+      const mockBody = {
+        email: 'rudolph@mail.com',
+        password: 'hashed_password',
+      } as LoginAuthenticationDto;
+
+      jest.spyOn(userService, 'findByEmail').mockResolvedValue(mockUser);
+
+      jest.spyOn(jwtService, 'signAsync').mockResolvedValue('userAccessToken');
+
+      const loginProcess = await controller.login(mockBody);
+
+      expect(loginProcess).toEqual(
+        expect.objectContaining({
+          access_token: expect.any(String),
+        })
+      );
+      expect(userService.findByEmail).toHaveBeenCalledWith(mockBody.email);
+      expect(verifyPassword).toHaveBeenCalledWith(
+        mockBody.password,
+        mockUser.password
+      );
+      expect(jwtService.signAsync).toHaveBeenCalled();
+    });
   });
 });
--- apps/backend/src/app/authentication/authentication.controller.ts
+++ apps/backend/src/app/authentication/authentication.controller.ts
@@ -11,13 +11,16 @@ import {
   RegisterAuthenticationResponseDto,
 } from './dto/register-authentication.dto';
 import { UserService } from '../user/user.service';
-import { hashPassword } from '../../utils/hash';
+import { hashPassword, verifyPassword } from '../../utils/hash';
+import { LoginAuthenticationDto } from './dto/login-authentication.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('authentication')
 export class AuthenticationController {
   constructor(
     private readonly authenticationService: AuthenticationService,
-    private readonly userService: UserService
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService
   ) {}
 
   @Post('register')
@@ -43,5 +46,35 @@ export class AuthenticationController {
     const formattedData = new RegisterAuthenticationResponseDto(user);
 
     return formattedData;
+  }
+
+  @Post('login')
+  async login(@Body() loginAuthDto: LoginAuthenticationDto) {
+    const user = await this.userService.findByEmail(loginAuthDto.email);
+
+    if (!user) {
+      throw new HttpException(
+        'The Email or Password is wrong.',
+        HttpStatus.UNAUTHORIZED
+      );
+    }
+
+    const isPasswordMatch = await verifyPassword(
+      loginAuthDto.password,
+      user.password
+    );
+
+    if (!isPasswordMatch) {
+      throw new HttpException(
+        'The Email or Password is wrong.',
+        HttpStatus.UNAUTHORIZED
+      );
+    }
+
+    const payload = { sub: user._id, name: user.name };
+
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return { access_token: accessToken };
   }
 }
